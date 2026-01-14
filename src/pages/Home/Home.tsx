756fcb53f0cb22b02b1709bc5ca39feb506b35e7
NatanaelDominguez <jorgedominguez349@gmail.com>
2026-01-14 19:51:47 -0300

    actualización, se consume una API para los productos, se utiliza useState y useEffect

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from "react"
 import { Hero } from "../../components/ui/Hero/Hero"
 
 const Home = () => {
+
+  const [products, setProducts] = useState([])
+  const getProducts = async () => {
+    try{
+      const response = await fetch('http://localhost:3000/products')
+      const data = await response.json()
+      setProducts(data)
+    }catch(error){
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getProducts()
+  },[])
+
+  console.log(products);
+  
   return (
     <>
       <Hero />
