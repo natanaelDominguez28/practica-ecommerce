8f14ff32c51df124078e029c581bce9d23e2b7fe
NatanaelDominguez <jorgedominguez349@gmail.com>
2026-03-20 16:09:19 -0300

    feat: cambio _limit por _per_page para el paginado

--- src/service/products.service.ts
+++ src/service/products.service.ts
@@ -2,7 +2,7 @@ import type { Product } from "../interface"
 
 export const getProducts = async (page=1): Promise<Product[]> => {   
     try{
-      const response = await fetch(`http://localhost:3000/products?_page=${page}&_limit=24`)
+      const response = await fetch(`http://localhost:3000/products?_page=${page}&_per_page=24`)
       if (response.ok){
         const data = await response.json()
         return data
